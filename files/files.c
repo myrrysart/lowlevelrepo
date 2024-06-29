cdb3855c74da6b78daad37f7e7adf94485aba4c7
myrrysart <jyry.niemitalo@gmail.com>
2024-06-29 20:05:40 +0300

    Files.c and restructuring files, 9

--- files/files.c
+++ files/files.c
@@ -1,25 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                             _  __    __    */
-/*   valgrindTest.c                                           /_\/ / /\ \ \   */
+/*   files.c                                                  /_\/ / /\ \ \   */
 /*                                                           //_\\ \/  \/ /   */
 /*   By: artwizard <hello@myrrys.art>                       /  _  \  /\  /    */
 /*                                                          \_/ \_/\/  \/     */
-/*   Created: 2024/06/09 09:19:56 by artwizard                                */
-/*   Updated: 2024/06/09 11:52:40 by artwizard                 myrrys.art     */
+/*   Created: 2024/06/29 19:29:56 by artwizard                                */
+/*   Updated: 2024/06/29 20:02:13 by artwizard                 myrrys.art     */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+ 
+int main (int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("usage: %s <filename>\n", argv[0]);
+		return (0);
+	}
+	int fd = open(argv[1], O_RDWR | O_CREAT, 0644);
+	if (fd == -1)
+	{
+		perror("open");
+		return(-1);
+	}
 
-struct employee_t{
-	char name[64];
-	int age;
-};
+	char *mydata = "Hello new file\n";
+	write(fd, mydata, strlen(mydata));
 
-int main(){
-
-	printf("%s, age %d\n", employee1.name, employee1.age);
-	return 0;
+	close(fd);	
+	return (0);
 }
